484184ddf9d2ca13a9040bd83c73ef4df63e3fb7
unknown <amawia15@gmail.com>
2020-11-21 14:15:22 +0530

    added

--- Website/js/scripts.js
+++ Website/js/scripts.js
@@ -1,38 +1,46 @@
 /*!
-    * Start Bootstrap - Creative v6.0.4 (https://startbootstrap.com/theme/creative)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
-    */
-(function($) {
+ * Start Bootstrap - Creative v6.0.4 (https://startbootstrap.com/theme/creative)
+ * Copyright 2013-2020 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
+ */
+(function ($) {
   "use strict"; // Start of use strict
 
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
       var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
       if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 72)
-        }, 1000, "easeInOutExpo");
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top - 72,
+          },
+          1000,
+          "easeInOutExpo"
+        );
         return false;
       }
     }
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
+  $(".js-scroll-trigger").click(function () {
+    $(".navbar-collapse").collapse("hide");
   });
 
   // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 75
+  $("body").scrollspy({
+    target: "#mainNav",
+    offset: 75,
   });
 
   // Collapse Navbar
-  var navbarCollapse = function() {
+  var navbarCollapse = function () {
     if ($("#mainNav").offset().top > 100) {
       $("#mainNav").addClass("navbar-scrolled");
     } else {
@@ -45,24 +53,43 @@
   $(window).scroll(navbarCollapse);
 
   // Magnific popup calls
-  $('#portfolio').magnificPopup({
-    delegate: 'a',
-    type: 'image',
-    tLoading: 'Loading image #%curr%...',
-    mainClass: 'mfp-img-mobile',
+  $("#portfolio").magnificPopup({
+    delegate: "a",
+    type: "image",
+    tLoading: "Loading image #%curr%...",
+    mainClass: "mfp-img-mobile",
     gallery: {
       enabled: true,
       navigateByImgClick: true,
-      preload: [0, 1]
+      preload: [0, 1],
     },
     image: {
-      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-    }
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+    },
   });
-
 })(jQuery); // End of use strict
 
-const upload = (e) => {
+const upload = async (e) => {
   e.preventDefault();
-  console.log(e);
-}
+  var file0 = e.target[0].files[1];
+  let reader = new FileReader();
+  var content = "";
+
+  reader.readAsText(file0);
+  reader.onload = function () {
+    content = reader.result;
+    console.log(content);
+  };
+
+  const node = await Ipfs.create();
+  // console.log("Your node: " + nodeId);
+  window.node = node;
+  const status = node.isOnline() ? "online" : "offline";
+  console.log(`Node status: ${status}`);
+  var files = [
+    {
+      path: file0.webkitRelativePath,
+      content,
+    },
+  ];
+};
